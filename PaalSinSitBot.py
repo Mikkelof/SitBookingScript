0231dd2bd019eade9622b30ced8d4edc68598a6e
KingShaggy <39702459+KingShaggy@users.noreply.github.com>
2021-02-09 22:34:59 +0100

    Gjort booking betydelig raskere

--- PaalSinSitBot.py
+++ PaalSinSitBot.py
@@ -42,44 +42,65 @@ loginKnapp.click()
 
 driver.get("https://www.sit.no/trening/treneselv")
 
+sleep(5)
 
 
-seksTretti_syvTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[2]/div"
-seks_syv = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[3]/div"
-syvTretti_aatteTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[4]/div"
-aatte_ni = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[5]/div"
-aatteTretti_niTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[6]/div"
-ni_ti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[7]/div"
-niTretti_tiTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[8]/div"
-ti_elleve = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[9]/div"
-tiTretti_elleveTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[10]/div"
-elleve_tolv = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[11]/div"
-elleveTretti_tolvTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[12]/div"
-tolv_tretten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[13]/div"
-tolvTretti_trettenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[14]/div"
-tretten_fjorten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[15]/div"
-trettenTretti_fjortenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[16]/div"
-fjorten_femten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[17]/div"
-fjortenTretti_femtenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[18]/div"
-femten_seksten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[19]/div"
-femtenTretti_sekstenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[20]/div"
-seksten_sytten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[21]/div"
-sekstenTretti_syttenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[22]/div"
-sytten_atten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[23]/div"
-syttenTretti_attenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[24]/div"
-atten_nitten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[25]/div"
-attenTretti_nittenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[26]/div"
-nitten_tjue = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[27]/div"
-nittenTretti_tjueTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[28]/div"
-tjue_tjueen = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[29]/div"
-tjueTretti_tjueenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[30]/div"
-tjueen_tjueto = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[31]/div"
-tjueenTretti_tjuetoTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[32]/div"
-tjueto_tjuetre = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[3]/div[33]/div"
+
+seksTretti_syvTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[2]/div"
+seks_syv = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[3]/div"
+syvTretti_aatteTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[4]/div"
+aatte_ni = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[5]/div"
+aatteTretti_niTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[6]/div"
+ni_ti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[7]/div"
+niTretti_tiTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[8]/div"
+ti_elleve = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[9]/div"
+tiTretti_elleveTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[10]/div"
+elleve_tolv = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[11]/div"
+elleveTretti_tolvTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[12]/div"
+tolv_tretten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[13]/div"
+tolvTretti_trettenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[14]/div"
+tretten_fjorten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[15]/div"
+trettenTretti_fjortenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[16]/div"
+fjorten_femten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[17]/div"
+fjortenTretti_femtenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[18]/div"
+femten_seksten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[19]/div"
+femtenTretti_sekstenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[20]/div"
+seksten_sytten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[21]/div"
+sekstenTretti_syttenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[22]/div"
+sytten_atten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[23]/div"
+syttenTretti_attenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[24]/div"
+atten_nitten = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[25]/div"
+attenTretti_nittenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[26]/div"
+nitten_tjue = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[27]/div"
+nittenTretti_tjueTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[28]/div"
+tjue_tjueen = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[29]/div"
+tjueTretti_tjueenTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[30]/div"
+tjueen_tjueto = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[31]/div"
+tjueenTretti_tjuetoTretti = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[32]/div"
+tjueto_tjuetre = "//*[@id=\"ScheduleApp\"]/div/div/div[4]/div[2]/div[33]/div"
 
 
 #Laget så den fjerner alt som ikke er Gløshaugen siden den r laget for meg, Pål og Ask og alle vi trenes på gløs, men dette kan endres på
 #Intill videre virker det kun på ukedager, ikke helger
+
+while True:
+    try:
+        driver.switch_to_frame(driver.find_element_by_id("ibooking-iframe"));
+        bookingKnapp1 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[2]/input")
+        bookingKnapp1.click()
+
+        bookingKnapp2 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[3]/input")
+        bookingKnapp2.click()
+
+        bookingKnapp3 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[4]/input")
+        bookingKnapp3.click()
+
+        bookingKnapp4 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[5]/input")
+        bookingKnapp4.click()
+    except:
+        driver.refresh
+    break        
+
 
 def book_time(maaned, dag, time, minutt, start_slutt_bokstaver):
     n = True
@@ -92,36 +113,18 @@ def book_time(maaned, dag, time, minutt, start_slutt_bokstaver):
         #FEMTE NUMMER ER MINUTTET (burde enten være 0 eller 30 siden det er da timene blir åpne for booking. Time 18:30 burde time være 18, min være 30)
         #SISTE NUMMER ER SEKUND. LA DEN STÅ SOM 1.
         if datetime.now().timestamp() >= datetime(2021, maaned, dag, time, minutt, 1).timestamp():
-            while True:
-                try:
-                    driver.refresh();
-                    sleep(7)
-                    driver.switch_to_frame(driver.find_element_by_id("ibooking-iframe"));
-                    bookingKnapp1 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[2]/input")
-                    bookingKnapp1.click()
-
-                    bookingKnapp2 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[3]/input")
-                    bookingKnapp2.click()
-
-                    bookingKnapp3 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[4]/input")
-                    bookingKnapp3.click()
-
-                    bookingKnapp4 = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[2]/div/button[5]/input")
-                    bookingKnapp4.click()
-                    #HER SKRIVER DU INN TIDSPUNKTET DU VIL BOOKE (TO DAGER FREM I TID, GITT DET ER EN UKEDAG)
-
-                    ferdigBook1 = driver.find_element_by_xpath(start_slutt_bokstaver)
-                    ferdigBook1.click()
-                    sleep(1)
-                    sisteBookKnapp = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[5]/div/div/div[3]/div[8]/button[1]")
-                    sisteBookKnapp.click()
-                    sleep(1)
-                    okKnapp = driver.find_element_by_xpath("//*[@id=\"ModalDiv\"]/div/div/div[2]/button")
-                    okKnapp.click()
-                    n = False
-                except:
-                    continue
-                break
+            nextKnapp = driver.find_element_by_id("next")
+            nextKnapp.click()
+            sleep(3)
+            ferdigBook1 = driver.find_element_by_xpath(start_slutt_bokstaver)
+            ferdigBook1.click()
+            sleep(1)
+            sisteBookKnapp = driver.find_element_by_xpath("//*[@id=\"ScheduleApp\"]/div/div/div[5]/div/div/div[3]/div[8]/button[1]")
+            sisteBookKnapp.click()
+            sleep(1)
+            okKnapp = driver.find_element_by_xpath("//*[@id=\"ModalDiv\"]/div/div/div[2]/button")
+            okKnapp.click()
+            n = False
         else:
             sleep(1)
 
@@ -130,6 +133,9 @@ def book_time(maaned, dag, time, minutt, start_slutt_bokstaver):
 #time er timen bookingen åpner (f.eks klokka 4 på ettermiddagen vil være 16)
 #minutt vil være enten 0 eller 30, avhengig av når du vil ha timen
 #start_slutt_bokstaver er start og slutttiden i bokstavform. (f.eks 16-17 vil være seksten_sytten)
+
+book_time(2, 9, 22, 00, tjueto_tjuetre)
+book_time(2, 9, 22, 00, tjueto_tjuetre)
 
 
 sleep(10)
